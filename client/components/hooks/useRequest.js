81b618bcf05df97753f1ea2a0181d9a2eb946370
AndyVo <andyvo190998@gmail.com>
2024-08-15 14:52:19 +0200

    update fetch hoook

--- client/components/hooks/useRequest.js
+++ client/components/hooks/useRequest.js
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export const useRequest = ({ url, method }) => {
-    const [errors, setErrors] = useState(null);
-    const doRequest = async () => {
-        axios[method](url)
-            .then(function (response) {
-                console.log(response.data);
-                return response.data;
-            })
-            .catch(function (error) {
-                setErrors(error);
-            });
-    };
+const useRequest = (url, method, options) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        console.log(method, url);
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await axios[method](url, options);
+                setData(response.data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    return { errors, doRequest };
+        fetchData();
+    }, [url, options]);
+
+    return { data, loading, error };
 };
+
+export default useRequest;
